03a375bb3a1e0fffdcadff26a0a646b125108477
ryotokuro <ryotokuro@gmail.com>
2018-12-04 13:36:23 +1100

    fixed condition block and parameterisation

--- Chapter 3 - Functions/collatzSequence.py
+++ Chapter 3 - Functions/collatzSequence.py
@@ -1,11 +1,11 @@
-def collatz():
+def collatz(number):
     if number % 2 == 0:
         # number is even
         return number // 2
 
     else:
         # number is odd
-        return 3 * (number+1)
+        return 3 * number + 1
 
 
 number = int(input())

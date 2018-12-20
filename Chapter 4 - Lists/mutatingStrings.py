475724b421c282880a355822ead2a07c4b306c07
ryotokuro <ryotokuro@gmail.com>
2018-12-20 22:58:59 +1100

    alt solution added

--- Chapter 4 - Lists/mutatingStrings.py
+++ Chapter 4 - Lists/mutatingStrings.py
@@ -6,3 +6,8 @@ print(location)  # printing just to make sure it works
 mutatedString = originalString[:location+2] + 'a lie.'
 
 print(mutatedString)
+
+# Alternatively
+originalString = 'The cake is not a lie.'
+
+mutatedString = originalString[:originalString.find('s+2')] + originalString[originalString.find('a'):]

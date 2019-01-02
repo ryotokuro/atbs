4e64c9cfcbd38a7a6f53050429bd9678807c9c90
ryotokuro <ryotokuro@gmail.com>
2019-01-02 23:58:26 +1100

    added problem

--- Chapter 4 - Lists/characterPictureGrid.py
+++ Chapter 4 - Lists/characterPictureGrid.py
@@ -7,3 +7,14 @@ grid = [['.', '.', '.', '.', '.', '.'],
  ['O', 'O', 'O', 'O', '.', '.'],
  ['.', 'O', 'O', '.', '.', '.'],
  ['.', '.', '.', '.', '.', '.']]
+
+# grid[x][y] are the x and y coordinates of the 2d grid drawn
+# [0][0] is the origin
+# need to print:
+# ..OO.OO..
+# .OOOOOOO.
+# .OOOOOOO.
+# ..OOOOO..
+# ...OOO...
+# ....O....
+

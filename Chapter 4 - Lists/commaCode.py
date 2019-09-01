f222fd693ae7cd4d3ee052ac35aca91d715e8ef4
ryotokuro <ryotokuro@gmail.com>
2019-09-01 17:28:37 +1000

    added comma exercise

---
+++ Chapter 4 - Lists/commaCode.py
@@ -0,0 +1,9 @@
+def commaCode(l):
+    return ', '.join(l[:-1]) + ' and ' + l[-1]
+
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+print(commaCode(spam))
+spam = ['sasquatch', 'godzilla', 'king king', 'loch ness', 'goblin', 'ghoul', 'a zombie with no conscience']
+print(commaCode(spam))
+print('Question: What do all these things have in common?')

79b89a8d5fec48be224b5cad9257415b8e792dd8
ryotokuro <ryotokuro@gmail.com>
2019-09-24 23:53:11 +1000

    added table printer program

---
+++ Chapter 6 - Manipulating Strings/tablePrinter.py
@@ -0,0 +1,15 @@
+# Write a function named printTable() that:
+# - takes a list of lists of strings
+# - and displays it in a well-organized table with each column right-justified.
+# Assume that all the inner lists will contain the same number of strings.
+
+# EXAMPLE
+# tableData = [['apples', 'oranges', 'cherries', 'banana'],
+#               ['Alice', 'Bob', 'Carol', 'David'],
+#               ['dogs', 'cats', 'moose', 'goose']]
+
+# OUTPUT
+# apples Alice dogs
+# oranges Bob cats
+# cherries Carol moose
+# banana David goose

736046042f83615fb12d22f37894f4f19e65dc5c
ryotokuro <ryotokuro@gmail.com>
2019-09-26 03:44:52 +1000

    added a function to parse a chunk of data and find any phone numbers in it

--- Chapter 6 - Manipulating Strings/isPhoneNumber.py
+++ Chapter 6 - Manipulating Strings/isPhoneNumber.py
@@ -1,10 +1,22 @@
 def isPhoneNumber(text):
-    print(text + ' is a phone number:', end=' ')
+    # print(text + ' is a phone number:', end=' ')
     
     # format XXX-XXX-XXXX
     return len(text) == 12 and (text[:3] + text[4:7] + text[8:]).isdecimal() and text[3] == text[7] == '-'
 
+def findNumber(message):
+    found = False
+    for i in range(len(message)):
+        chunk = message[i:i+12]
+        if isPhoneNumber(chunk):
+            print('Phone number found:', chunk)
+            found = True
+    if not found:
+        print('No phone number found.')
+    print('Done.')
 
-print(isPhoneNumber('415-555-4242'))
-print(isPhoneNumber('Moshi moshi'))
-print(isPhoneNumber('000-000-00001'))
+# print(isPhoneNumber('415-555-4242'))
+# print(isPhoneNumber('Moshi moshi'))
+# print(isPhoneNumber('000-000-00001'))
+findNumber('Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.')
+findNumber('Here\'s my number: 315-643-283')

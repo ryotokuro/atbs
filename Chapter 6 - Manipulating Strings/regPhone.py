42ec38270cd1e3920f427ebbd96b0330635ce328
ryotokuro <ryotokuro@gmail.com>
2019-09-27 00:00:16 +1000

    added mo object unpack

--- Chapter 6 - Manipulating Strings/regPhone.py
+++ Chapter 6 - Manipulating Strings/regPhone.py
@@ -7,4 +7,6 @@ import re  # import regex module
 
 # r signals to use RAW strings (disregards the \)
 phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
-
+string = 'My phone number is 941-436-7273!'
+mo = phoneNumRegex.search(string)  # finds and creates match object
+print('Phone number found: ' + mo.group())

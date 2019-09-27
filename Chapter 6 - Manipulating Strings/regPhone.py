da15bb5d2f6c5b2b076e4655f5c74929fbe89677
ryotokuro <ryotokuro@gmail.com>
2019-09-27 14:56:02 +1000

    added use of different parameters and functions i.e. findall()

--- Chapter 6 - Manipulating Strings/regPhone.py
+++ Chapter 6 - Manipulating Strings/regPhone.py
@@ -14,11 +14,53 @@ print('Area code found:', mo.group(1))
 print('Phone number found:', mo.group(2))
 print('Full number:', mo.group())
 
+# having different options |
 heroRegex = re.compile(r'Spiderman|Superman')
 heroBase = heroRegex.findall('Spidermane vs. Superman')
 print(heroBase[0], heroBase[1])
 
+# having multiple compound words (|)
 batRegex = re.compile(r'Bat(mobile|car|plane|signal|copter|man)')
 batFind = batRegex.search('batman has a Batmobile and a batplan')
+print(batFind.group(), batFind.group(1))
+
+# matching zero or one times ()?
+batRegex = re.compile(r'Bat(wo)?man')
+batFind = batRegex.search('Batman is a new man')
+print(batFind.group())
+batFind = batRegex.search('Batwoman is a new (wo)man')
 print(batFind.group())
 
+# matching zero or more times ()*
+batRegex = re.compile(r'Bat(wo)*man')
+batFind = batRegex.search('Batwowowowoman')
+print(batFind.group())
+
+# matching AT LEAST ONCE or MORE times ()+
+batRegex = re.compile(r'Bat(MAN)+ loves')
+batFind = batRegex.search('Bat loves pizza')
+print(batFind)
+batFind = batRegex.search('BatMAN loves pizza')
+print(batFind.group())
+
+# specific number of repeats
+batRegex = re.compile(r'(na){8,24} batman!')
+batFind = batRegex.search('nananananananananananananananananananananananana batman!')
+print(batFind.group())
+
+# non-greedy match
+batRegex = re.compile(r'(na){8,24}?')
+batFind = batRegex.search('nananananananananananananananana batman!')
+print(batFind.group())
+
+# findall() method
+string = 'Home: (54) 4524-331 | Work: (61) 9342-100'
+phoneRegex = re.compile(r'\(\d{2}\) \d{4}-\d{3}')
+phoneFind = phoneRegex.findall(string)
+print('Phone:', phoneFind[0], phoneFind[1])
+
+string = 'Home: 9473-323 | Work: (61) 2344-234'
+phoneRegex = re.compile(r'(\(\d{2}\))? (\d{4}-\d{3})')
+phoneFind = phoneRegex.findall(string)
+# print(phoneFind)
+print('Phone:', *phoneFind[0], *phoneFind[1])

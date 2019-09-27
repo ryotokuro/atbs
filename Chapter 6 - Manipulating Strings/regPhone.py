e36393ecc813babb01e99ef1b1347460ed289bb5
ryotokuro <ryotokuro@gmail.com>
2019-09-27 13:17:11 +1000

    added different uses of piping and grouping with match objects using regex

--- Chapter 6 - Manipulating Strings/regPhone.py
+++ Chapter 6 - Manipulating Strings/regPhone.py
@@ -6,7 +6,19 @@
 import re  # import regex module
 
 # r signals to use RAW strings (disregards the \)
-phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
-string = 'My phone number is 941-436-7273!'
+phoneNumRegex = re.compile(r'(\(\d{3}\)) (\d{3}-\d{4})|\d{3}-\d{3}-\d{4}')
+string = 'My phone number is (111) 111-1111 and 222-222-2222'
 mo = phoneNumRegex.search(string)  # finds and creates match object
-print('Phone number found: ' + mo.group())
+print(mo)
+print('Area code found:', mo.group(1))
+print('Phone number found:', mo.group(2))
+print('Full number:', mo.group())
+
+heroRegex = re.compile(r'Spiderman|Superman')
+heroBase = heroRegex.findall('Spidermane vs. Superman')
+print(heroBase[0], heroBase[1])
+
+batRegex = re.compile(r'Bat(mobile|car|plane|signal|copter|man)')
+batFind = batRegex.search('batman has a Batmobile and a batplan')
+print(batFind.group())
+

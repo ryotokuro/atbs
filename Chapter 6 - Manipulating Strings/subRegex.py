33cf5741068243a9a80bf5fc86bc7679178e93bb
ryotokuro <ryotokuro@gmail.com>
2019-10-09 23:55:50 +1100

    added a complete censor using sub that loses information'

--- Chapter 6 - Manipulating Strings/subRegex.py
+++ Chapter 6 - Manipulating Strings/subRegex.py
@@ -4,3 +4,7 @@ import re
 code_regex = re.compile(r'Agent \w+', re.I)
 code_names = code_regex.sub('CENSORED', 'Agent John is reporting to AGENT Smith - the alpha virus of the Matrix.')
 print(code_names)
+
+code_regex = re.compile(r'Agent (\w)\w*')
+code_names = code_names = code_regex.sub(r'\1***', 'The agent of secret stuff is actually Agent Ryan and Agent Arden')
+print(code_names)

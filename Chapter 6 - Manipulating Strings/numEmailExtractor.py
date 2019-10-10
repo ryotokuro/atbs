46de892494b31055c0c415b882d97b7f490b4f3b
ryotokuro <ryotokuro@gmail.com>
2019-10-10 23:57:47 +1100

    added more support for different types of email addresses

--- Chapter 6 - Manipulating Strings/numEmailExtractor.py
+++ Chapter 6 - Manipulating Strings/numEmailExtractor.py
@@ -1,3 +1,6 @@
+#! python3
+# phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
+
 import re
 import pyperclip as py
 
@@ -8,21 +11,27 @@ text = py.paste()
 # find all phone numbers
 phoneFilter = re.compile(r'''(
                         (\d{3}|\(\d{3}\))?  # area code
-                        (\s|-)?
-                        (\d{3})
-                        (\s|-)
-                        (\d{4})
+                        (\s|-|\.)?          # separator
+                        (\d{3})             # 3 digits
+                        (\s|-|\.)           # separator
+                        (\d{4})             # 4 digits
                         )''', re.VERBOSE)
 phoneBuffer = phoneFilter.findall(text)
 if phoneBuffer:
     print(phoneBuffer)
     # phoneList = ', '.join(phoneBuffer)
-    # py.copy(phoneList)
+    # py.copy(phoneList) 
 else:
     print('No phone numbers were found in the text :(')
 
 # find all emails 
-emailFilter = re.compile(r'\w+@\w+\.com', re.I)
+emailFilter = re.compile(r'''(
+                        [a-zA-Z0-9._%+-]+   # username
+                        @  # @ symbol
+                        [a-zA-Z0-9.-]+  # domain
+                        (\.[a-zA-Z])  # tld
+                        (\.[a-zA-Z])?  # area code
+                        )''', re.I | re.VERBOSE)
 emailsBuffer = emailFilter.findall(text)
 if emailsBuffer:
     emailList = ', '.join(emailsBuffer)

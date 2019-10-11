de66ead5d575d01fb050f665f99374800590a881
ryotokuro <ryotokuro@gmail.com>
2019-10-11 11:02:27 +1100

    fixed email filter and phone filter, rearranged to save them as strings but at the moment the second filter overrides the clipboard, so the alternative is matching it for both:

--- Chapter 6 - Manipulating Strings/numEmailExtractor.py
+++ Chapter 6 - Manipulating Strings/numEmailExtractor.py
@@ -7,36 +7,43 @@ import pyperclip as py
 # get the text off the clipboard
 text = py.paste()
 
-# regex magix 788-2233 and 234-2343
+# regex magix (133) 788-2233 and 234-2343
 # find all phone numbers
 phoneFilter = re.compile(r'''(
-                        (\d{3}|\(\d{3}\))?  # area code
-                        (\s|-|\.)?          # separator
+                        (\d{3}(\s|-|\.)?|\(\d{3}\)(\s|-|\.)?)?  # area code + separator
                         (\d{3})             # 3 digits
                         (\s|-|\.)           # separator
                         (\d{4})             # 4 digits
                         )''', re.VERBOSE)
-phoneBuffer = phoneFilter.findall(text)
-if phoneBuffer:
-    print(phoneBuffer)
-    # phoneList = ', '.join(phoneBuffer)
-    # py.copy(phoneList) 
-else:
+
+numbers = []
+for groups in phoneFilter.findall(text):
+    numbers.append(groups[0])
+
+if not numbers:  # if empty list
     print('No phone numbers were found in the text :(')
+else:
+    numbers = '\n'.join(numbers)
+    py.copy(numbers) 
 
 # find all emails 
 emailFilter = re.compile(r'''(
                         [a-zA-Z0-9._%+-]+   # username
                         @  # @ symbol
                         [a-zA-Z0-9.-]+  # domain
-                        (\.[a-zA-Z])  # tld
-                        (\.[a-zA-Z])?  # area code
+                        (\.[a-zA-Z]+)  # tld
+                        (\.[a-zA-Z]+)?  # area domain
                         )''', re.I | re.VERBOSE)
-emailsBuffer = emailFilter.findall(text)
-if emailsBuffer:
-    emailList = ', '.join(emailsBuffer)
-    py.copy(emailList.lower())  # convert to lower and copy to clipboard
-else:
+
+emails = []
+for groups in emailFilter.findall(text):
+    emails.append(groups[0])
+
+if not emails:
     print('No emails were found in the text :(')
+else:
+    emails = '\n'.join(emails)
+    py.copy(emails.lower())  # convert to lower and copy to clipboard
+    
 
 

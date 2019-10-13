e7e1954d0ede076e762845afb98b71fce395c604
ryotokuro <ryotokuro@gmail.com>
2019-10-13 12:01:57 +1100

    added directory creation using makedir

--- Chapter 8 - Reading & Writing Files/os_path.py
+++ Chapter 8 - Reading & Writing Files/os_path.py
@@ -1,8 +1,8 @@
 import os
 
 # adjusts depending on what OS the script is running on
-# raw_path = 'C:\\Users\\autum\\Desktop\\Pizza'
-raw_path = '..\\..'  # navigating backwards (relative)
+raw_path = 'C:\\Users\\autum\\Desktop\\Pizza'
+# raw_path = '..\\..'  # navigating backwards (relative)
 
 # path = os.path.join(raw_path) for os dependent config
 
@@ -14,8 +14,11 @@ try:
     os.chdir(raw_path)
     print("After:", os.getcwd())
 except FileNotFoundError:  # if it doesn't exist
-    print("Let's try create the directory then!")
-    
+    print("Directory does not exist. Creating directory...")
+    os.makedirs(raw_path)
+    os.chdir(raw_path)
+    print(os.getcwd())
 
-os.chdir('atbs')
-print(os.getcwd())
+#os.chdir('atbs')
+#print(os.getcwd())
+#os.makedirs('.\\Pizza')

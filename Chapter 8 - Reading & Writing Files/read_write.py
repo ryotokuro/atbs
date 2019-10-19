844eade8dd1d192cd143110e7a79e874799dc7fb
ryotokuro <ryotokuro@gmail.com>
2019-10-19 23:48:45 +1100

    added writeable mode

--- Chapter 8 - Reading & Writing Files/read_write.py
+++ Chapter 8 - Reading & Writing Files/read_write.py
@@ -6,3 +6,13 @@ print(text_file.read())
 new_file = open("C:\\Users\\autum\\OneDrive\\Documents\\atbs\\Chapter 8 - Reading & Writing Files\\cheese.txt", 'r')
 l = new_file.read().split('\n')
 print(l)
+
+# overwrite file content write()
+file = ".\\hello.txt"
+text_file = open(file, 'r')  # then open in read mode
+initial_content = 
+text_file = open(".\\hello.txt", 'w')  # open as writeable file
+text_file.write("hello")
+text_file = open(".\\hello.txt", 'r')  # then open in read mode
+okay = text_file.read()
+print(okay)
